5405d2621a374405884e1e640a1e0c28c7a3e3e6
xuguojing-xgj <2313493661@qq.com>
2022-08-20 12:33:25 +0800

    submit 提交事件 prevent修饰符 阻止页面刷新

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,12 @@
     <div v-else-if="flag == 'B'"> 这是b</div>
     <div v-else-if="flag == 'C'"> 这是c</div>
     <div v-else> 这是d</div>
+
+    <button @click="clickFn">btn事件</button>
+
+    <form action="/">
+        <button @click.prevent="submitFn" type="submit"> 提交 </button>
+    </form>
 </template>
 
 <script setup lang="ts" >
@@ -15,6 +21,15 @@ const message: string = '我,是,小,憨,批'
 const message1: string = '<div>我,是,小,憨,批</div>'
 
 const flag: string = ''
+
+const clickFn = () => {
+    console.log('触发了点击事件');
+}
+
+const submitFn = (e: Object) => {
+    console.log(e);
+
+}
 </script>
 
 <style scoped>
