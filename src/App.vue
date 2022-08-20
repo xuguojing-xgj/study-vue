db35f48c0fe2c495e7a5602dce7e246747ef28f0
xuguojing-xgj <2313493661@qq.com>
2022-08-20 11:02:02 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="app">
+        <!-- <h3>我是app组件(祖)</h3> -->
+        div
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+.app {
+    background-color: #ccc;
+    width: 500px;
+    height: 500px;
+}
+</style>
