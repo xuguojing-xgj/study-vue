53b583822950c9d107872a930e3b99ea2a4fa899
xuguojing-xgj <2313493661@qq.com>
2022-08-20 12:21:41 +0800

    v-if v-else-if v-else

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
     <div>
-        <div  v-text="message"></div>
-        <div  v-html="message1"></div>
-        <!-- {{ message.split(',').map(x => `&${x}`) }} -->
+        <div v-text="message"></div>
+        <div v-html="message1"></div>
     </div>
+
+    <div v-if="flag == 'A'"> 这是a</div>
+    <div v-else-if="flag == 'B'"> 这是b</div>
+    <div v-else-if="flag == 'C'"> 这是c</div>
+    <div v-else> 这是d</div>
 </template>
 
 <script setup lang="ts" >
 const message: string = '我,是,小,憨,批'
 const message1: string = '<div>我,是,小,憨,批</div>'
+
+const flag: string = ''
 </script>
 
 <style scoped>
