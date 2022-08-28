8ee08533947541c61783bbe0315e637bd060921d
xuguojing-xgj <2313493661@qq.com>
2022-08-28 10:42:32 +0800

    toRef,toRefs,toRaw

--- src/02-reactive使用.vue
+++ src/02-reactive使用.vue
@@ -3,11 +3,15 @@
         <!-- submit 提交事件 prevent修饰符 阻止页面刷新 -->
         reactive
         <p>{{ message }}</p>
+        <p>{{ val }}</p>
+        <!-- * shallowReactive 挂载之后不会更新深层次视图 比如 Object类型  -->
+        <button @click="change1">btn①</button>
+        <button @click="change2">btn②</button>
     </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, readonly, shallowReactive, isReactive } from 'vue'
 
 let message = reactive<Array<number>>([]) // 接收一个复杂类型的数据
 
@@ -34,8 +38,57 @@ setTimeout(() => {
     msg.list = arr
 
     console.log(msg);
-    
+
 }, 1000)
+
+
+// readonly copy只读
+let person = reactive({
+    count: 1
+})
+person.count++
+let copy = readonly(person)
+// 这里是不能修改数据的
+// copy.count++
+
+
+
+/**
+ * shallowReactive 挂载之前 跟踪 更新视图 
+ * shallowReactive 挂载之后不会更新深层次视图 比如 Object类型 
+ */
+
+let val = shallowReactive({
+    test: '我是小明',
+    nav: {
+        bar: {
+            name: '我是小红'
+        }
+    }
+})
+
+const change1 = () => {
+    val.test = '我是小明 被改啦'
+}
+const change2 = () => {
+    val.nav.bar.name = '我是小红 被改啦 nav-bar-name'
+    console.log(val);
+
+}
+/**
+ * shallowReactive 挂载之前 跟踪 更新视图 
+ * change1()
+ * change2()
+ */
+
+//  import {
+//     shallowReactive,
+//     isReactive
+// } from 'https://unpkg.com/@vue/reactivity/dist/reactivity.esm-browser.js'
+// isReactive 判断是不是一个reactive 复杂类型的响应式数据 返回值 boolean
+const props = shallowReactive({ n: 1 })
+console.log(isReactive(props.n)) // false
+console.log(isReactive(props)) // true
 </script>
 
 <style>
