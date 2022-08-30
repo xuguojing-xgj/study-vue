f31d4069066085d43ab7ea4d4f5b09eb633ceec1
xuguojing-xgj <2313493661@qq.com>
2022-08-30 21:01:05 +0800

    子向父传值

--- src/layout/content/index.vue
+++ src/layout/content/index.vue
@@ -1,13 +1,32 @@
 <script lang="ts" setup>
-// 定义一个接口类型
+import { reactive } from "@vue/reactivity"
+
+
+/**
+ * defineProps接收 父组件 传过来的值
+ * 定义一个接口类型
+ */
 interface Props {
     title: string,
     dataList: number[]
 }
-/**
- * defineProps接收 父组件 传过来的值
- */
 defineProps<Props>()
+
+
+/**
+ * 子向父传值
+ * defineEmits() 返回一个函数 接收一个数组['事件名称']
+ * emit() 返回函数的 第二个参数 要传递的数据
+ * const emit = defineEmits<{
+ * (event: 'change'): void
+ * (event: 'update', id: number): void
+ * }>()
+ */
+const list = reactive<string[]>(['1', '2', '3'])
+const emit = defineEmits(['on-click'])
+const change = () => {
+    emit('on-click', list)
+}
 </script>
 
 
@@ -16,7 +35,9 @@ defineProps<Props>()
         <div class="div"> 我是 content 组件
 
             <p>{{  title  }}</p>
-            <p>{{ dataList }}</p>
+            <p>{{  dataList  }}</p>
+
+            <button @click="change">子向父</button>
         </div>
 
     </div>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,14 +1,23 @@
 <script lang="ts" setup>
 import content from './content/index.vue'
 import { reactive } from 'vue'
+/**
+ *  向父组件 传递的数据
+ */
 let list = reactive<number[]>([1, 2, 3, 4])
+/**
+ * 获取子组件 传递过来的数据
+ */
+const getSonList = (list: string[]) => {
+    console.log(list);
+}
 </script>
 
 
 <template>
     <div class="box">
-        <!-- 向子组件传值 动态数组  title -->
-        <content :dataList="list" title="我想买一条裤子" />
+        <!--子向父 @事件名称    父向子组件传值 :动态数组  -->
+        <content @on-click="getSonList" :dataList="list" title="我想买一条裤子" />
         <div class="div">我是 index 页面</div>
     </div>
 </template>
