3bd20cb650160a191f6e3ef74070b0b75ba20c7b
xuguojing-xgj <2313493661@qq.com>
2022-09-04 01:46:18 +0800

    updata file

---
+++ src/03-插槽.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import MyDialog from './components/Dialog/index.vue'
+</script>
+
+<template>
+    <div>
+        <MyDialog>
+            <template v-slot:MyHeader>
+                <div>我是Header 具名插槽</div>
+            </template>
+            <template v-slot>
+                <div>我是Main 匿名插槽</div>
+            </template>
+            <template #MyFooter>
+                <div>我是MyFooter 具名插槽</div>
+            </template>
+        </MyDialog>
+    </div>
+</template>
+
+<style>
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
-import MyDialog from './components/Dialog/index.vue'
+
 </script>
 
 <template>
     <div>
-        <MyDialog>
-            <template v-slot:MyHeader>
-                <div>我是Header 具名插槽</div>
-            </template>
-            <template v-slot>
-                <div>我是Main 匿名插槽</div>
-            </template>
-            <template #MyFooter>
-                <div>我是MyFooter 具名插槽</div>
-            </template>
-        </MyDialog>
+
     </div>
 </template>
 
