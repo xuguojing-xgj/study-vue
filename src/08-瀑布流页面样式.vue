5cbd60cd63bbd46ea35b2a90dae207d7f05590a5
xuguojing-xgj <2313493661@qq.com>
2023-03-24 17:17:51 +0800

    update files

---
+++ src/08-瀑布流页面样式.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import waterFall from './components/water-fall/water-fall.vue'
+
+const list = [
+    {
+        height: 300,
+        background: 'pink',
+    },
+    {
+        height: 200,
+        background: 'red',
+    },
+    {
+        height: 100,
+        background: 'blue',
+    },
+    {
+        height: 400,
+        background: 'skyblue',
+    },
+    {
+        height: 200,
+        background: 'green',
+    },
+    {
+        height: 200,
+        background: 'green',
+    },
+    {
+        height: 400,
+        background: 'skyblue',
+    },
+    {
+        height: 300,
+        background: 'pink',
+    },
+    {
+        height: 200,
+        background: 'red',
+    },
+    {
+        height: 100,
+        background: 'blue',
+    },
+    {
+        height: 200,
+        background: 'green',
+    },
+    {
+        height: 200,
+        background: 'green',
+    },
+    {
+        height: 400,
+        background: 'skyblue',
+    },
+]
+</script>
+
+<template>
+    <waterFall :list="list"></waterFall>
+</template>
+
+
+
+<style lang="scss" scoped></style>
