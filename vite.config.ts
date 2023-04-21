0b623f33f1223d4703217195d244856ed2ba72f0
xuguojing-xgj <2313493661@qq.com>
2023-04-21 11:12:39 +0800

    install pinia

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
+import * as path from 'path';
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
     plugins: [vue(),
     // ...
     AutoImport({
