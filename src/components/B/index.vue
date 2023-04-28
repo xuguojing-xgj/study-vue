94e2c3070936ab94c3df82f9415365ae25e68947
xuguojing-xgj <2313493661@qq.com>
2023-04-28 17:01:47 +0800

    mitt 全局注册 使用

--- src/components/B/index.vue
+++ src/components/B/index.vue
@@ -1,11 +1,23 @@
 <script setup lang='ts'>
+// 使用全局注册的 mitt
+import { getCurrentInstance } from 'vue'
 
+const instance = getCurrentInstance()
+
+const emit = () => {
+    /** 
+     * emit('自定义事件', 参数)
+     * 发送多条
+     * */
+    instance?.proxy?.$Bus.emit('on-string', '小明')
+    instance?.proxy?.$Bus.emit('on-number', 1)
+}
 </script>
 
 <template>
     <div>
         <h2>B组件</h2>
-        <button type="button">emit</button>
+        <button type="button" @click="emit">emit</button>
     </div>
 </template>
 
--- src/components/C/index.vue
+++ src/components/C/index.vue
@@ -1,5 +1,31 @@
 <script setup lang='ts'>
+// 使用全局注册的 mitt
+import { getCurrentInstance } from 'vue'
 
+const instance = getCurrentInstance()
+
+const Bus = (str: any) => {
+    console.log(str);
+}
+// 接收事件      on('自定义参数', () => {})
+instance?.proxy?.$Bus.on('on-string', Bus)
+
+/**
+ * 接收多条
+ */
+
+instance?.proxy?.$Bus.on('*', (type, data) => {
+    console.log(type, data);
+})
+
+// 删除方法 
+/**
+ * off('事件名称', 删除的函数)
+ * all.clear() 删除全部
+ */
+// instance?.proxy?.$Bus.off('on-string', Bus)
+
+// instance?.proxy?.$Bus.all.clear()
 </script>
 
 <template>
