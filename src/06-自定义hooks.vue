1bc5cfa5a974a2fdd8817b752db97fc4f00bc889
xuguojing-xgj <2313493661@qq.com>
2023-06-02 17:18:49 +0800

    自定义hooks

---
+++ src/06-自定义hooks.vue
@@ -0,0 +1,37 @@
+<script setup lang='ts'>
+import { ref } from 'vue'
+import useBase64 from './hooks'
+// import A from '@/components/A/index.vue'
+import Atemplate from './components/A/index.vue'
+const arr = [
+    {
+        id: '1',
+        name: '小明',
+    },
+    {
+        id: '2',
+        name: '小红',
+    },
+    {
+        id: '3',
+        name: '小紫',
+    },
+]
+useBase64({
+    el: '#img'
+}).then((res) => {
+    console.log(res.baseUrl);
+})
+
+
+</script>
+
+<template>
+    <div>
+        <Atemplate :list="arr" title="我是一个字符串" :num="1"></Atemplate>
+        <div>{{ 'app' }}</div>
+        <img src="@/assets/1.jpg" id="img" alt="">
+    </div>
+</template>
+
+<style lang='scss' scoped></style>
--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,27 @@
 <script setup lang='ts'>
-import { ref } from 'vue'
-import useBase64 from './hooks'
-// import A from '@/components/A/index.vue'
-import Atemplate from './components/A/index.vue'
-const arr = [
-    {
-        id: '1',
-        name: '小明',
-    },
-    {
-        id: '2',
-        name: '小红',
-    },
-    {
-        id: '3',
-        name: '小紫',
-    },
-]
-useBase64({
-    el: '#img'
-}).then((res) => {
-    console.log(res.baseUrl);
+import { onMounted, ref } from 'vue'
+import useResize from 'dom-change-plugin'
 
+
+
+onMounted(() => {
+    const el = document.querySelector('.container')
+    useResize(el, (e: any) => {
+        console.log(e);
+    })
 })
-
 
 </script>
 
 <template>
-    <div>
-        <Atemplate :list="arr" title="我是一个字符串" :num="1"></Atemplate>
-        <div>{{ 'app' }}</div>
-        <img src="@/assets/1.jpg" id="img" alt="">
+    <div class="container">
     </div>
 </template>
 
-<style lang='scss' scoped></style>
+<style lang='less' scoped>
+.container {
+    width: 200px;
+    height: 200px;
+    background-color: yellow;
+}
+</style>
