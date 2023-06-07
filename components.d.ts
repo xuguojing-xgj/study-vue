7112ea5e5ee20db2092f7b808ab68d3ccf287f99
xuguojing-xgj <2313493661@qq.com>
2023-06-07 15:45:42 +0800

    update files

--- components.d.ts
+++ components.d.ts
@@ -12,6 +12,7 @@ declare module '@vue/runtime-core' {
     C: typeof import('./src/components/C/index.vue')['default']
     Card: typeof import('./src/components/Card/index.vue')['default']
     Dialog: typeof import('./src/components/Dialog/index.vue')['default']
+    ElInput: typeof import('element-plus/es')['ElInput']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     Skeleton: typeof import('./src/components/expame/skeleton.vue')['default']
---
+++ src/07-侦听dom变化.vue
@@ -0,0 +1,27 @@
+<script setup lang='ts'>
+import { onMounted, ref } from 'vue'
+import useResize from 'dom-change-plugin'
+
+
+
+onMounted(() => {
+    const el = document.querySelector('.container') as HTMLElement
+    useResize(el, (e: any) => {
+        console.log(e);
+    })
+})
+
+</script>
+
+<template>
+    <div class="container">
+    </div>
+</template>
+
+<style lang='less' scoped>
+.container {
+    width: 200px;
+    height: 200px;
+    background-color: yellow;
+}
+</style>
